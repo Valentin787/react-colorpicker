ac5be0571465383749bbc0b4dc08a2f4476bc8dc
Valentin787 <89661234+Valentin787@users.noreply.github.com>
2022-05-26 12:38:15 +0300

    day_5_shoulComponentUpdate/PureComponent

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './ColorPicker.css'
 
 
-class ColorPicker extends Component{
+class ColorPicker extends PureComponent{
   
   static propTypes = {
     value: PropTypes.array.isRequired,
@@ -14,9 +14,26 @@ class ColorPicker extends Component{
     value: this.props.colors,
     activeOptionsInd:this.props.number,
   }
+
+  // SHOULD/COMPONENT/UPDATE 
+  //Мы либо используем метод shouldComponentUpdate либо PureComponent при обьявлении класса ^
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // console.log(`nextProps`, nextProps);
+  //   // console.log(`nextState`, nextState);
+  //   const { activeOptionsInd } = this.state;
+
+  //   return nextState.activeOptionsInd !== activeOptionsInd
+  // }
+
+  // CLICK 
+
   handleClick = (index) => {
    this.setState({activeOptionsInd:index})
   }
+
+  // MAKE CLASSES
+
   makeOptionsClasess = (index) => {
   const finalArr = ['button']
 
@@ -25,9 +42,15 @@ class ColorPicker extends Component{
     }
       return finalArr.join(' ')
   }
+  
+  // RENDER
 
   render() {
-    const { value ,activeOptionsInd} = this.state;
+    console.log(`Re-render @ ${Date.now()}`);
+    const { value, activeOptionsInd } = this.state;
+    const { colors } = this.props;
+    // const { label } = colors[activeOptionsInd]
+    
 
     return (
       <div className="container">
